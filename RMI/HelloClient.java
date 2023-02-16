1d049c81a65946a9e0574b2d2a5664a593e8ce71
AmeyaKulkarni2001 <ameya.2000k@gmail.com>
2023-02-16 14:34:18 +0530

    Multimachine Code added

--- RMI/HelloClient.java
+++ RMI/HelloClient.java
@@ -4,7 +4,7 @@ import java.rmi.*;
 public class HelloClient {
     public static void main(String[] argv) {
         try {
-            HelloInterface hello = (HelloInterface) Naming.lookup("//localhost/Hello");
+            HelloInterface hello = (HelloInterface) Naming.lookup("//172.16.188.63/Hello"); //serveripaddr
             System.out.println(hello.say());
         } catch (Exception e) {
             System.out.println("HelloClient exception:" + e);
--- RMI/HelloServer.java
+++ RMI/HelloServer.java
@@ -4,6 +4,7 @@ import java.rmi.*;
 public class HelloServer {
     public static void main(String[] argv) {
         try {
+            System.setProperty("java.rmi.server.hostname", "172.16.188.63"); //ipaddr of server
             Hello robj = new Hello("Hello, World!");
             Naming.rebind("Hello", robj);
             System.out.println("Hello Server is ready.");
